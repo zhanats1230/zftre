a1a8b3dc85b87bb51247b29abee7bbca12a1c31f
zhanats1230 <99787708+zhanats1230@users.noreply.github.com>
2025-02-10 00:31:11 +0600

    Update server.js

--- server.js
+++ server.js
@@ -450,7 +450,6 @@ fetch("/getPumpSettings")
   <label for="pumpDuration">Время работы (сек):</label>
   <input type="number" id="pumpDuration" min="1">
 
-  <label for="pumpInterval">Интервал между включениями (мин):</label>
   <input type="number" id="pumpInterval" min="1">
 
   <button onclick="savePumpSettings()">Сохранить</button>
