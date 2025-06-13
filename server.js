edcdd4af08af2723bfff65ef50234b740ab5fcd5
zhanats1230 <99787708+zhanats1230@users.noreply.github.com>
2025-06-13 14:48:41 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1377,7 +1377,15 @@ document.getElementById('soilMoistureChartBtn').addEventListener('click', () => 
         const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
         const passwordSection = document.getElementById('passwordSection');
         const controlSection = document.getElementById('controlSection');
-
+const video = document.getElementById('camStream');
+  if (video) {
+    video.onerror = () => {
+      video.poster = 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="red"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z"/></svg>';
+      video.style.width = '100px';
+      video.style.height = '100px';
+      console.error('Ошибка загрузки видеопотока');
+    };
+  }
         if (isLoggedIn) {
           passwordSection.classList.add('hidden');
           controlSection.classList.remove('hidden');
